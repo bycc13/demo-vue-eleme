f18efc346b43e36d326ab2f60c742e04b4c9abd4
bycc13 <bycc13@163.com>
2018-01-19 15:11:17 +0800

    from vue doc example

--- src/components/good/good.vue
+++ src/components/good/good.vue
@@ -1,12 +1,105 @@
 <template>
-  <div >this is goods</div>
+  <div>
+    <div class="pva-select">
+      <div class="pva-sleect__tags">
+        <span></span>
+        <input type="text">
+      </div>
+      <div class="pva-input is-focus">
+        <input type="text" class="pva-input__inner">
+        <span></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      newTodoText: '',
+      todos: [
+        {
+          id: 1,
+          title: 'todo1'
+        },
+        {
+          id: 2,
+          title: 'todo2'
+        },
+        {
+          id: 3,
+          title: 'todo3'
+        },
+        {
+          id: 4,
+          title: 'todo4'
+        }
+      ],
+      nextTodoId: 5
+    }
+  },
+  methods: {
+    addNewTag () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" rel="stylesheet/scss">
+  .pva-select {
+    position: relative;
+    width: 240px;
+    .pva-sleect__tags {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    .pva-tag {
+      margin: 3px 0 3px 6px;
+      box-sizing: border-box;
+      border-color: transparent;
+      background-color: #f0f2f5;
+    }
+    .pva-input {
+      display: block;
+      position: relative;
+      font-size: 14px;
+      width: 100%;
+      &--suffix {
+
+
+      }
+      &__inner {
+        padding-left: 30px;
+      }
+      &.is-focus {
+        .pva-input__inner {
+          border-color: #409EFF;
+        }
+      }
+    }
+  }
+  .pva-tag {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 12px;
+    line-height: 21px;
+    border-radius: 4px;
+    border: 1px solid rgba(64,158,255,.2);
+    white-space: pre-line;
+    word-break: break-all;
+    // &--info {
+    //   box-sizing: border-box;
+    //   margin: 3px 0 3px 6px;
+    //   background-color: #f0f2f5;
+    // }
+  }
+
+
 
 </style>
--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -1,9 +1,129 @@
 <template>
-  <div >this is rating</div>
+  <div >
+    <button 
+      @click="justClick()" 
+      :class="[
+        { 'active': active }
+      ]" 
+    >
+      just click
+    </button>
+
+    <div>{{msg}}</div>
+    <div 
+      v-text="msg" 
+      :href="msg"
+    >
+      <span>v-text</span>
+    </div>
+
+    <input type="text" v-model="msg">
+
+    <ul>
+      <li 
+        v-for="(item, index) in items" 
+        :key="item.id" 
+      >
+        {{item}} + {{index}}
+      </li>
+    </ul>
+
+    <template v-if="loginType === 'username'">
+      <label>Username</label>
+      <input placeholder="Enter your username" key="username-input">
+    </template>
+    <template v-else>
+      <label>Email</label>
+      <input placeholder="Enter your email address" key="email-input">
+    </template>
+
+    <template>
+      <label>Username</label>
+      <input v-show="active" placeholder="Enter your username" >
+    </template>
+
+    <ul>
+      <li 
+        v-for="item in person" 
+        :key="item.index" 
+        :id="item.index" 
+      >
+        {{item.name}} - {{item.email}}
+        <a 
+          href="javascript:;" 
+          @click="deleteItem(item)"
+        >
+          delete
+        </a>
+      </li>
+    </ul>
+
+    <button @click="filterBtn()">filterdd</button>
+
+    <ul>
+      <template >
+        <li v-for="(item, index) in items" :key="index" :href="index" v-if="item === 2">{{item}}</li>
+      </template> 
+    </ul>
+
+    <dialog open>
+      <p>Greetings, one and all!</p>
+    </dialog>
+
+  </div>
+  
 </template>
 
 <script>
 export default {
+  name: 'product',
+  data () {
+    return {
+      msg: 'teee',
+      items: [1, 2, 3, 52554, 5],
+      active: false,
+      loginType: 'username',
+      person: [
+        {
+          name: 'kacj',
+          email: 'kacj@163.com',
+          age: 325
+        },
+        {
+          name: 'yoona',
+          email: 'yoona@163.com',
+          age: 2
+        },
+        {
+          name: 'ty',
+          email: 'ty@163.com',
+          age: 8
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteItem (item) {
+      this.person.splice(item, 1)
+    },
+    justClick () {
+      this.active = !(this.active)
+      if (this.loginType === 'username') {
+        this.loginType = 'email'
+      } else {
+        this.loginType = 'username'
+      }
+      this.$nextTick(function () {
+        console.log('$nextTick中打印：', this.$el.children[1].innerText)
+      })
+      console.log('直接打印：', this.$el.children[1].innerText)
+    },
+    filterBtn () {
+      this.person = this.person.filter(function (item) {
+        return item.match(/2/)
+      })
+    }
+  }
 }
 </script>
 
